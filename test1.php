560897fcf78ca7d26c45ac197ebe048190b404d8
Duvier Reinosa <duvierreinosa@gmail.com>
2022-03-28 23:26:37 -0500

    test 1

--- test1.php
+++ test1.php
@@ -2,6 +2,16 @@
 
 function is_palindrome($input) {
     //solve this method that says if the sentence is palindrome or not
+    if (strlen($cadena)<2) {
+        return false;
+    }
+    $cadena=strtolower(str_replace([" ", ",", "."], "", $cadena));
+    for ($i=0;$i<strlen($cadena);$i++){
+        if($cadena[$i] != $cadena[strlen($cadena)-$i-1]){
+            return false;
+        }
+    }
+    return true;
 }
 
 $input = "Allí por la tropa portado, traído a ese paraje de maniobras, una tipa como capitán usar boina me dejara, pese a odiar toda tropa por tal ropilla";
