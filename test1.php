63365e73e28e0049e2b54f6f79c327a38b6153dc
Duvier Reinosa <duvierreinosa@gmail.com>
2022-03-29 10:51:24 -0500

    update test 1

--- test1.php
+++ test1.php
@@ -2,16 +2,13 @@
 
 function is_palindrome($input) {
     //solve this method that says if the sentence is palindrome or not
-    if (strlen($cadena)<2) {
+    $input=strtolower(str_replace([" ", ",", "."], "", $input));
+    $invert = strrev($input);
+    if($input == $invert){
+        return true;
+    }else{
         return false;
     }
-    $cadena=strtolower(str_replace([" ", ",", "."], "", $cadena));
-    for ($i=0;$i<strlen($cadena);$i++){
-        if($cadena[$i] != $cadena[strlen($cadena)-$i-1]){
-            return false;
-        }
-    }
-    return true;//if don´t enter in other returns is because is a palindrome
 }
 
 $input = "Allí por la tropa portado, traído a ese paraje de maniobras, una tipa como capitán usar boina me dejara, pese a odiar toda tropa por tal ropilla";
