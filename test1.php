9de5f8ac56a28334e0968189539ac469a869fe8e
Duvier Reinosa <duvierreinosa@gmail.com>
2022-03-29 15:23:02 -0500

    test 1 and 4 ready

--- test1.php
+++ test1.php
@@ -1,4 +1,8 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 
 function is_palindrome($input) {
     //solve this method that says if the sentence is palindrome or not
@@ -11,6 +15,12 @@ function is_palindrome($input) {
         return false;
     }
 }
+//for test one
+// $input = "Allí por la tropa portado, traído a ese paraje de maniobras, una tipa como capitán usar boina me dejara, pese a odiar toda tropa por tal ropilla";
+// var_dump(is_palindrome($input));
 
-$input = "Allí por la tropa portado, traído a ese paraje de maniobras, una tipa como capitán usar boina me dejara, pese a odiar toda tropa por tal ropilla";
-var_dump(is_palindrome($input));
+//for http request
+$_POST = json_decode(file_get_contents('php://input'), true);
+var_dump(is_palindrome($_POST['input']));
+
+
--- test4.jsx
+++ test4.jsx
@@ -1,12 +1,67 @@
 import React from 'react';
 import axios from 'axios';
 
-export default class Palindrome extends React.Component {
+//styles
 
+const divCss = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginTop: '44px'
+  }
+  const inputCss = {
+    height: '30px',
+    borderRadius: '25px',
+    border: 'solid 1px #3975ff',
+    textIndent: '10px',
+    margin: '22px'
+  }
+  const BtnCss = {
+    height: '30px',
+    borderRadius: '25px',
+    border: 'solid 1px #3975ff',
+    background: '#3975ff',
+    width: '100px',
+    color: 'white'
+  }
+
+
+export default class Palindrome extends React.Component {
+    state = {
+        input: '',
+        isPalindrome: ''
+    }
+
+    InputChange = (event) => {
+        this.setState({input: event.target.value});
+    }
+
+     checkIs = async () => {
+        await axios.post(`http://localhost/test1.php`,{"input": this.state.input})
+        .then(resp =>{
+            if(Boolean(resp.data.substring(resp.data.length - 6 ,resp.data.length - 2))){
+                this.setState({isPalindrome: 'Is a Palindrome'});
+            }else{
+                this.setState({isPalindrome: 'Is not a Palindrome'});
+            }
+        }).catch(err =>{
+            console.log(err);
+        });
+    }
+    
     render() {
         return (
-            <>
-            </>
+            <div style={divCss}>
+                <h1>Palindrome</h1>
+                <input 
+                    placeholder='Enter the phrase'
+                    type="text"
+                    style={inputCss} 
+                    value={this.state.input}
+                    onChange={this.InputChange}/>
+                <button style={BtnCss} onClick={this.checkIs}>Check</button>
+                {this.state.isPalindrome == ''? <></> : <h3>{this.state.isPalindrome}</h3>}
+            </div>
         )
     }
 }
